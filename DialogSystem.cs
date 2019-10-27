4d20ca3de64f56f3e6d095e0f3fd7c507b19ebcc
Gasbebe <foo897@naver.com>
2019-10-28 00:11:39 +0900

    add DialogSystem.cs

---
+++ DialogSystem.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogSystem : MonoBehaviour
+{
+    public Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+
+    void Start()
+    {
+        sentences = new Queue<string>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null; 
+        }
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("find of conversation");
+    }
+}
+
+[SerializeField]
+public class Dialogue
+{
+    public string name;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+}
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogSystem>().StartDialogue(dialogue);
+    }
+}
