5100b41f6a5df201d89113de2e09c26d2d1bce94
lhh <foo897@naver.com>
2023-06-26 16:54:05 +0900

    Update plist_parser.py

--- plist_parser.py
+++ plist_parser.py
@@ -82,3 +82,74 @@ button.pack()
 
 root.mainloop()
 #t.join()
+
+
+
+
+
+
+#============
+import plistlib
+import re
+from dataclasses import dataclass
+
+@dataclass
+class Position:
+    x : str = None
+    y : str = None
+
+class d_parse:
+    def __init__(self) -> None:
+        pass
+    
+    def load(self, path):
+        with open(path, "rb") as fp:
+            print("file path : {}", path)
+            self.info = plistlib.load(fp)
+    
+    def get_frames(self):
+        return self.info["frames"]
+    
+    def get_metadata(self):
+        return self.info["metadata"]
+    
+    #{0,0}
+    def get_position(self, pos_str : str):
+        find_list = re.findall(r"\{[\d]+,\s*[\d]+\}", pos_str)
+        pos_list = []
+        for item in find_list :
+            coordinates = item.strip("{}")
+            pos = Position()
+            pos.x, pos.y = coordinates.replace(" ", "").split(",")
+            pos_list.append(pos)
+            # []
+        return pos_list
+#=================
+from d_parser import *
+import os
+import re
+
+def main():
+    # ss = re.findall(r"\{[\d]+,\s*[\d]+\}", "gleeee")
+    parse = d_parse()
+    parse.load(os.path.join(os.getcwd(), "resources", "fx", "f3_fx_circleofdessication.plist"))
+    print(parse.get_frames())
+    print(parse.get_metadata())
+    
+    for frame_info in parse.get_frames().values():
+        test = frame_info["frame"]
+        # pos_list = test.split(",") 
+        # print(test)
+        # print(parse.get_position(test))
+        for item in parse.get_position(test):
+            print("===================")
+            print(item.x)
+            print(item.y)
+        
+        # for pos in pos_list:
+        #     # print(pos)
+        #     print(parse.get_position(pos))
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
